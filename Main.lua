3869eacf4f120a9b81c7f3354493f48bcf71831a
Nameless <dev@name1ess.ga>
2022-09-21 21:42:50 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -6,15 +6,17 @@ repeat
     wait()
 until game:IsLoaded()
 
-syn.queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/Nameless9000/ChatBypass/main/Main.lua'))()")
+shared.optionalWords=shared.optionalWords or false
+
+syn.queue_on_teleport("shared.optionalWords="..(shared.optionalWords and "true" or "false")..";loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Nameless9000/ChatBypass/main/Main.lua'))()")
 
 shared.ACL = false
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Nameless9000/ChatBypass/main/AntiChatLog.lua'))()
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Nameless9000/ChatBypass/main/AntiChatLog.lua'))()
 if shared.ACL == true then
     messagebox("Script cannot run!", "Bypassing is not possible on this game.", 0)
     game:Shutdown()
     return
 end
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Nameless9000/ChatBypass/main/Bypasser.lua'))()
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Nameless9000/ChatBypass/main/Bypasser.lua'))()
