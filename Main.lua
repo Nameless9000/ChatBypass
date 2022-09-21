cf7a4c4d123979c0660f0c57728ae9b0d56f698e
Nameless <dev@name1ess.ga>
2022-09-21 22:00:46 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -12,11 +12,13 @@ syn.queue_on_teleport("shared.optionalWords="..(shared.optionalWords and "true" 
 
 shared.ACL = false
 
-loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Nameless9000/ChatBypass/main/AntiChatLog.lua'))()
-if shared.ACL == true then
-    messagebox("Script cannot run!", "Bypassing is not possible on this game.", 0)
-    game:Shutdown()
-    return
-end
+--loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Nameless9000/ChatBypass/main/AntiChatLog.lua'))()
+--if shared.ACL == true then
+--    messagebox("Script cannot run!", "Bypassing is not possible on this game.", 0)
+--    game:Shutdown()
+--    return
+--end
+
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/AnthonyIsntHere/anthonysrepository/main/scripts/Anti%20Chat%20Logger.lua'))()
 
 loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Nameless9000/ChatBypass/main/Bypasser.lua'))()
